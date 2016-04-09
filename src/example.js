90f536c9660852794ea2aec9bb5f4e86803a7402
shigemk2 <i.am.shige@gmail.com>
2016-04-09 22:33:56 +0900

    Add sample
    アプリが動いているlocalforageはずっと続くようだ

--- src/example.js
+++ src/example.js
@@ -1,8 +1,22 @@
 var localforage = require("localforage");
-localforage.setItem('key', 'value');
-console.log(localforage.getItem('key'));
-localforage.getItem('key', function (err, value) {
+localforage.setItem('one', '1');
+console.log(localforage.getItem('one'));
+localforage.getItem('one', function (err, value) {
   console.log(value);
+});
+localforage.iterate(function(value, key, iterationNumber) {
+    console.log([key, value]);
+}).then(function() {
+    console.log('Iteration has completed');
+});
+
+
+localforage.setItem('two', '2');
+localforage.setItem('three', '3');
+localforage.iterate(function(value, key, iterationNumber) {
+    console.log([key, value]);
+}).then(function() {
+    console.log('Iteration has completed');
 });
 
 
