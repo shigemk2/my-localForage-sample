074f1d3b5ab4cefd782cf26719d74ac74d390ecf
shigemk2 <i.am.shige@gmail.com>
2016-04-09 22:32:00 +0900

    Add sample

--- src/example.js
+++ src/example.js
@@ -1,1 +1,16 @@
-console.log("hogehoge");
+var localforage = require("localforage");
+localforage.setItem('key', 'value');
+console.log(localforage.getItem('key'));
+localforage.getItem('key', function (err, value) {
+  console.log(value);
+});
+
+
+
+
+
+
+
+
+
+
